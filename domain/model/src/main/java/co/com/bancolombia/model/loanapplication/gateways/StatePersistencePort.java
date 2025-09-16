65a8031f5dd8daeae85614581cf6200eeb61b5cd
Ruben Dario Barrios Montealegre <rubenbarmont@gmail.com>
2025-09-16 10:35:57 -0500

    Commit inicial para la rama feature/HU02_solicitudes_pruebas

--- domain/model/src/main/java/co/com/bancolombia/model/loanapplication/gateways/StatePersistencePort.java
+++
@@ -1,9 +0,0 @@
-package co.com.bancolombia.model.loanapplication.gateways;
-
-import co.com.bancolombia.model.loanapplication.model.StateModel;
-import reactor.core.publisher.Mono;
-
-public interface StatePersistencePort {
-    Mono<StateModel> findStateByName(String name);
-    Mono<StateModel> findStateById(Long id);
-}
